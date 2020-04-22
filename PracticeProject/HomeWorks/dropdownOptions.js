ddbade316d4f2207ab59c38d6feb33d75754db8b
Mihai <40312079+xtremer15@users.noreply.github.com>
2020-04-23 00:27:35 +0300

    Add files via upload

---
+++ PracticeProject/HomeWorks/dropdownOptions.js
@@ -0,0 +1,30 @@
+export default DORPDOWN_ELEMENTS = [
+    {
+      elementToDisplayOnNavBar: "Developers",
+      submenuForElement: [
+        {
+          subOpt1: ["Install", "Tutorial", "Doc"],
+          subOpt2: ["FAQ", "Forum", "Showcase"],
+        },
+      ],
+    },
+    {
+      elementToDisplayOnNavBar: "Hosting",
+      submenuForElement: [
+        {
+          subOpt: ["Pricing", "Plans", "More..."],
+        },
+      ],
+    },
+    {
+      elementToDisplayOnNavBar: "Company",
+      submenuForElement: [
+        {
+          subOpt: ["Carrera", "Culture", "Partners", "Blog"],
+        },
+      ],
+    },
+    {
+      elementToDisplayOnNavBar: "Sign Up",
+    },
+  ];
